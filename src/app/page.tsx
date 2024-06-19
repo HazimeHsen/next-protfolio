6347a544331738da49c268727f42cefc0be7d77d
Hsen-123 <hazimehussein01@gmail.com>
2024-06-19 21:26:31 +0300

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
     <>
       <Navbar />
       <Hero />
-      <div className="absolute -top-[0px] left-1/2 -translate-x-1/2">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 ">
         <Moon />
       </div>
       <>
