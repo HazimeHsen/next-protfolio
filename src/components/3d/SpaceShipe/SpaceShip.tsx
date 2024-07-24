1b09574f6cbdabceb9763005d5114c68e30552be
Hsen-123 <hazimehussein01@gmail.com>
2024-07-25 02:27:49 +0300

    changes

--- src/components/3d/SpaceShipe/SpaceShip.tsx
+++ src/components/3d/SpaceShipe/SpaceShip.tsx
@@ -112,7 +112,7 @@ const SpaceShip: React.FC<SpaceShipProps> = ({
         height: "100vh",
       }}>
       <ambientLight intensity={1} />
-      <pointLight position={[0, 0, 20]} />
+      <pointLight position={[0, 0, 0]} />
       <Suspense fallback={<Loader />}>
         <Modal
           isLoading={isLoading}
