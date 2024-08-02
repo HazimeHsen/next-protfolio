e116def0c16c26b9d4c6773bc32a07061671f4c6
Hsen-123 <hazimehussein01@gmail.com>
2024-08-03 02:25:03 +0300

    add google analytics

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -12,6 +12,7 @@ import Moon from "@/components/3d/Moon";
 import ThreeScene from "@/components/3d/StartAnimation";
 import Highlights from "@/sections/Highlights";
 import Projects from "@/sections/Projects";
+import Script from "next/script";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -55,28 +56,39 @@ export default function Home() {
       )}
       <AnimatePresence>
         {showContent && (
-          <>
-            <motion.div
-              key="content"
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              transition={{ duration: 1 }}
-              className="relative z-20">
-              <Navbar />
-              <Hero />
-              <div className="fixed inset-0">
-                <Moon />
-              </div>
-              <Highlights />
-              <Projects />
-              <ExperienceSection />
-              <About />
-              <Contact />
-            </motion.div>
-          </>
+          <motion.div
+            key="content"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 1 }}
+            className="relative z-20">
+            <Navbar />
+            <Hero />
+            <div className="fixed inset-0">
+              <Moon />
+            </div>
+            <Highlights />
+            <Projects />
+            <ExperienceSection />
+            <About />
+            <Contact />
+          </motion.div>
         )}
       </AnimatePresence>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-8SNS7WXM1Y"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-8SNS7WXM1Y');
+        `}
+      </Script>
     </div>
   );
 }
