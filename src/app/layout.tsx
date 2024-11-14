1ac5dcefd0f4322bea3f0e9c9c4635b667369e7e
HazimeHsen <hazimehussein01@gmail.com>
2024-11-14 13:36:32 +0200

    fix metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,22 +9,45 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: "Hsen | Portfolio",
+  title: "Hussein Hazime | Full-Stack Developer Portfolio",
   description:
-    "Welcome to Hsen's portfolio, showcasing my skills and projects as a Next.js developer. Explore my work in web development, including modern technologies like Tailwind CSS, Three.js, and React Three Fiber. Discover my journey, experience, and the innovative solutions I've crafted.",
+    "Explore the portfolio of Hussein Hazime, a passionate full-stack developer specializing in React.js, Node.js, and MongoDB. Discover projects, skills, and the journey in web development.",
+  openGraph: {
+    title: "Hussein Hazime | Full-Stack Developer Portfolio",
+    description:
+      "Explore the portfolio of Hussein Hazime, a passionate full-stack developer specializing in React.js, Node.js, and MongoDB. Discover projects, skills, and the journey in web development.",
+    url: "https://hsen.me",
+    siteName: "Hussein Hazime Portfolio",
+    images: [
+      {
+        url: "https://hsen.me/_next/image?url=%2Fpersonal%2Fme.jpg&w=384&q=100",
+        width: 1200,
+        height: 630,
+        alt: "Hussein Hazime Portfolio",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Hussein Hazime | Full-Stack Developer Portfolio",
+    description:
+      "Explore the portfolio of Hussein Hazime, a passionate full-stack developer specializing in React.js, Node.js, and MongoDB. Discover projects, skills, and the journey in web development.",
+    images: ["https://hsen.me/_next/image?url=%2Fpersonal%2Fme.jpg&w=384&q=100"],
+  },
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body
-        className={
-          poppins.className + " min-h-screen overflow-y-auto overflow-x-hidden"
-        }>
+        className={`${poppins.className} min-h-screen overflow-y-auto overflow-x-hidden`}
+      >
         {children}
       </body>
     </html>
